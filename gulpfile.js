edc00e6ff1b3a269511a1c03c72ec1b39fcab01d
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2018-04-12 12:43:06 -0400

    Iniciamos

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp'),
+	postcss = require('gulp-postcss'),
+	  cssnext = require('postcss-cssnext'),
+	  atImport = require('postcss-import'),
+	  cssnano = require('cssnano'),
+	  mqpacker = require('css-mqpacker'),
+    fontMagician = require('postcss-font-magician'),
+	browserSync = require('browser-sync').create()
+
+// Servidor de desarrollo
+gulp.task('serve', () => {
+	browserSync.init({
+    server: './'
+  });
+ })
+gulp.task('css', ()=>{
+
+  const processor = [
+    atImport(),
+    fontMagician({
+      variants: {
+          'Ubuntu': {
+            '500': [],
+            '700': []
+          },
+          'Montserrat': {
+            '400': [],
+            '600': []
+          }
+        }
+      }),
+    //cssnested,
+    cssnext({browsers: ['> 5%', 'ie 8']}),
+    mqpacker(),
+    //cssnano()
+  ]
+  return gulp.src('./src/estilos.css')
+    //.pipe(sourcemaps.init())
+    .pipe(postcss(processor))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream())
+})
+
+// Tarea para vigilar los cambios
+gulp.task('watch', ()=>{
+	gulp.watch('./src/*.css', ['css'])
+  gulp.watch('./*.html').on('change', browserSync.reload)
+	gulp.watch('./js/*.js').on('change', browserSync.reload)
+})
+gulp.task('default',['watch','serve'])
---
+++ pull_request_template.md
@@ -0,0 +1,13 @@
+# ¿Que ha cambiado?
+
+[] HTML
+[] CSS
+[] Otros
+
+# ¿De que proyecto?
+
+[] Maquetacion
+[] Estilos
+[] Responsive
+
+# ¿Como podemos visualizar los cambios?
